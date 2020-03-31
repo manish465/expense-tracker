e1044658fe2dd0b2342ed09a71368da54f424c79
manish465 <ms5978307@gmail.com>
2020-03-31 13:46:32 -0700

    added updated list by input

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -4,6 +4,12 @@ const AddTransaction = props => {
     var tag = "manish";
     var amount = 788;
 
+    const getText = event => {
+        tag = event.target.value;
+    };
+    const getAmount = event => {
+        amount = event.target.value;
+    };
     const handelSubmit = event => {
         event.preventDefault();
     };
@@ -14,13 +20,21 @@ const AddTransaction = props => {
             <form onSubmit={handelSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text" />
+                    <input
+                        type="text"
+                        onChange={getText}
+                        placeholder="Enter text"
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">
                         Amount <br />
                     </label>
-                    <input type="number" placeholder="Enter Amount" />
+                    <input
+                        type="number"
+                        onChange={getAmount}
+                        placeholder="Enter Amount"
+                    />
                 </div>
                 <button className="btn" onClick={() => props.add(tag, amount)}>
                     Add Transaction
