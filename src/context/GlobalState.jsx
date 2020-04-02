e2154444bcfc7ba1a16046d19dcda01e9e5d0198
manish465 <ms5978307@gmail.com>
2020-04-02 10:26:11 -0700

    added another transection

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -7,6 +7,7 @@ const initialState = {
         { id: 2, text: "Salary", amount: 300 },
         { id: 3, text: "Book", amount: -10 },
         { id: 4, text: "Camera", amount: 150 },
+        { id: 4, text: "loan", amount: 520 },
     ],
 };
 
