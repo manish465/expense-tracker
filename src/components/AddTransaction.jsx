633dce48ffdf9067086cc0fffa33c1b890e4262a
manish465 <ms5978307@gmail.com>
2020-04-02 11:48:36 -0700

    add transaction fn

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const AddTransaction = () => {
     const [text, setText] = useState("");
     const [amount, setAmount] = useState(0);
 
+    const { addTransaction } = useContext(GlobalContext);
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const transaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text: text,
+            amount: +amount,
+        };
+        addTransaction(transaction);
+    };
+
     return (
         <React.Fragment>
             <h3>Add New Transaction</h3>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input
