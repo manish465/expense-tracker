0dcf5b75ce045740072f4bf4f5a1f37c220fa73b
manish465 <ms5978307@gmail.com>
2020-04-06 13:37:40 -0700

    build

---
+++ build/precache-manifest.ef59b946e55403ead3a80720edbf035a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561828d60c0ce52f59b4f6418bd2fedb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b57e7f3ac94ec41bdc91",
+    "url": "/static/css/main.e6b6dd6d.chunk.css"
+  },
+  {
+    "revision": "02d65ad6ec437f72acf1",
+    "url": "/static/js/2.070f1458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.070f1458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57e7f3ac94ec41bdc91",
+    "url": "/static/js/main.227ece6b.chunk.js"
+  },
+  {
+    "revision": "3e5e20c52f8e9bacf785",
+    "url": "/static/js/runtime-main.b6a55b0e.js"
+  }
+]);
